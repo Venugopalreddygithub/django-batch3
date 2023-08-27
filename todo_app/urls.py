48b7093068998e216e90c151038a7e6efbea6de2
venugopal <bvenugopalreddy16@gmail.com>
2023-08-27 10:26:55 +0530

    todo list render page

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include 
-from todo_app.views import index, bye, hello 
-
+from todo_app.views import index
 urlpatterns = [
     path('index/', index, name='todo_index'),
-    path('bye/', bye, name='todo_bye'),
-    path('hello/', hello, name='todo_hello'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import redirect, render 
 from django.http import HttpResponse, JsonResponse 
-from todo_app.models import Todo 
+from todo_app.models import Todo
+
 
 def index(request):
-    todo = Todo.objects.last()
-    html = f"""
-        <h1>Hello World </h1>
-        <p>Bye</p>
-        <p>Todod title: {todo.title}</p>
-""" 
-    return HttpResponse(html)
-
-def bye(request):
-    todo = Todo.objects.last()
-    a = { 
-        "title": todo.title,
-        "author": "Nepoleon Hill"
+    all_todos = Todo.objects.all()
+    data = {
+        "todo": all_todos
     }
-    return JsonResponse(a)
-
-def hello(request):
-    todos = Todo.objects.all()
-    page_name = "temp.html"
-    return render(request, page_name, context={'todos': todos, 'show': True})
+    return render(request, "index.html", data)
