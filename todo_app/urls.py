f92f2a82a106e8f6ac23932b95cf9f1e3da2b1e3
venugopal <bvenugopalreddy16@gmail.com>
2023-08-28 19:31:20 +0530

    todo app changes

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include 
-from todo_app.views import index
+from todo_app.views import index, add_view, detailed_view, delete_todo
 urlpatterns = [
-    path('index/', index, name='todo_index'),
+    path('', index, name='todo_index'),
+    path('add-todo/', add_view, name='add-todo'),
+    path('detailed-view/<int:todo_id>/', detailed_view, name='detailed_view'),
+    path('delete-todo/<int:todo_id>', delete_todo, name="delete_todo")
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -4,8 +4,45 @@ from todo_app.models import Todo
 
 
 def index(request):
-    all_todos = Todo.objects.all()
+    all_todos = Todo.objects.all().order_by('-created_at')
     data = {
         "todo": all_todos
     }
     return render(request, "index.html", context=data)
+
+def add_view(request):
+    if request.method == "GET":
+        return HttpResponse("Invalid Method")
+    else:
+        todo_input = request.POST['todoInput']
+        print("todo_input:", todo_input)
+        Todo.objects.create(title=todo_input)
+        return redirect('todo_index')
+
+def detailed_view(request, todo_id):
+    if request.method == "POST":
+        return HttpResponse("Invalid Method")
+    else:
+        try:
+            todo_object = Todo.objects.get(id=todo_id)
+            print(todo_object)
+            data = {
+                'id': todo_object.id,
+                'title': todo_object.title,
+                'completed': todo_object.completed,
+                'created_at': todo_object.created_at,
+                'updated_at': todo_object.updated_at,
+            }
+            return JsonResponse(data)
+        except Todo.DoesNotExist:
+            return HttpResponse("Error Todo not found")
+def delete_todo(request, todo_id):
+        if request.method == "GET":
+            return HttpResponse("Invalid Method")
+        else:
+            try:
+                todo_object = Todo.objects.get(id=todo_id)
+                todo_object.delete()
+                return redirect('todo_index')
+            except Todo.DoesNotExist:
+                return HttpResponse("Error Todo not found")
